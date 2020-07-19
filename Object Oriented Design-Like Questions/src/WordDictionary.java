2ea3dab9d27743e8e730e0fd9f5515bf528f052e
ALEXJI19991007 <yu.ji@vanderbilt.edu>
2020-07-19 14:53:57 +0800

    add problems

---
+++ Object Oriented Design-Like Questions/src/WordDictionary.java
@@ -0,0 +1,51 @@
+public class WordDictionary {
+    static class TrieNode {
+        TrieNode[] child;
+        boolean isWord;
+
+        public TrieNode(){
+            this.child = new TrieNode[26];
+            this.isWord = false;
+        }
+    }
+
+    private final TrieNode root;
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+            if (node.child[ch - 'a'] == null) {
+                node.child[ch - 'a'] = new TrieNode();
+            }
+            node = node.child[ch - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return searchHelper(word, root, 0);
+    }
+
+    private boolean searchHelper(String word, TrieNode node, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        if (word.charAt(index) != '.') {
+            return node.child[word.charAt(index) - 'a'] != null && searchHelper(word, node.child[word.charAt(index) - 'a'], index + 1);
+        }
+        for (TrieNode n : node.child) {
+            if (n != null && searchHelper(word, n, index + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
