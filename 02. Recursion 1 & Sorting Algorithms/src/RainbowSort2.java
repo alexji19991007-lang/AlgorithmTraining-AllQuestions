801e28849206d2309f5c4218a4b05eab270c05b2
ALEXJI19991007 <yu.ji@vanderbilt.edu>
2021-03-18 09:14:05 -0500

    rainbow sort

--- 02. Recursion 1 & Sorting Algorithms/src/RainbowSort2.java
+++ 02. Recursion 1 & Sorting Algorithms/src/RainbowSort2.java
@@ -3,40 +3,33 @@ import java.util.*;
 public class RainbowSort2 {
     public static void main(String[] args) {
         int[] array = {2, 3, 3, 1, 2, 3, 0, 1, 3, 0};
-        rainbowSortII(array);
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(rainbowSortII(array)));
     }
 
-    public static void rainbowSortII(int[] array) {
-        // Physical meaning of ix:
-        // All numbers to the left of ix are all sorted and smaller than x.
-        int i0 = 0;
-        int i1 = 0;
-        int i2 = 0;
-        int i3 = 0;
-        while(i3 < array.length) {
-            if(array[i3] == 3) {
-                i3++;
-            } else if(array[i3] == 2) {
-                swap(array, i2, i3);
-                i2++;
-                i3++;
-            } else if(array[i3] == 1) {
-                swap(array, i2, i3);
-                swap(array, i1, i2);
-                i1++;
-                i2++;
-                i3++;
+    public static int[] rainbowSortII(int[] array) {
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+        int a = 0, b = 0, c = 0, d = array.length - 1;
+        while (c <= d) {
+            if (array[c] == 3) {
+                swap(array, c, d);
+                d--;
+            } else if (array[c] == 0) {
+                swap(array, c, b);
+                swap(array, b, a);
+                a++;
+                c++;
+                b++;
+            } else if (array[c] == 1) {
+                swap(array, b, c);
+                b++;
+                c++;
             } else {
-                swap(array, i2, i3);
-                swap(array, i1, i2);
-                swap(array, i0, i1);
-                i0++;
-                i1++;
-                i2++;
-                i3++;
+                c++;
             }
         }
+        return array;
     }
 
     public static void swap(int[] array, int a, int b) {
--- 02. Recursion 1 & Sorting Algorithms/src/RainbowSort3.java
+++ 02. Recursion 1 & Sorting Algorithms/src/RainbowSort3.java
@@ -1,13 +1,40 @@
+import java.util.Arrays;
+
 public class RainbowSort3 {
+    public static void main(String[] args) {
+        RainbowSort3 test = new RainbowSort3();
+        System.out.println(Arrays.toString(test.rainbowSortIII(new int[]{1, 3, 4, 2, 5, 2, 1}, 5)));
+    }
+
     public int[] rainbowSortIII(int[] array, int k) {
-        int[] pointers = new int[k + 1];
-        while(pointers[k] < array.length) {
-            int target = array[pointers[k]];
-            for(int i = k; i - 1 >= target; i--){
-                swap(array, pointers[i], pointers[i - 1]);
-            }
-            for(int i = k; i >= target; i--) {
-                pointers[i]++;
+        if (k == 1) {
+            return array;
+        }
+        int[] pointers = new int[k];
+        // Two qualities:
+        // From pointers[0] to pointers[k - 2]:
+        //         All elements to the left of pointers[i] should be less than k + 1;
+        // For pointers[k - 1]:
+        //         All elements to the right of pointers[k - 1] should be k.
+        //
+        // The array region from index 0 to pointers[k - 2] & from index
+        // pointers[k - 2] to pointers[k - 1] must satisfy the above two qualities.
+        // The array region from index pointers[k - 2] to pointers[k - 1] is unexplored.
+        pointers[k - 1] = array.length - 1;
+        while (pointers[k - 2] <= pointers[k - 1]) {
+            int target = array[pointers[k - 2]];
+            if (target == k) {
+                swap(array, pointers[k - 2], pointers[k - 1]);
+                pointers[k - 1]--;
+            } else {
+                // From the pointers[k - 2], swap until the above two qualities restore
+                for (int i = k - 2; i >= target; --i) {
+                    swap(array, pointers[i], pointers[i - 1]);
+                }
+                // For every pointer that experiences a swap, move ahead by one step
+                for (int i = k - 2; i >= target - 1; --i) {
+                    pointers[i]++;
+                }
             }
         }
         return array;
